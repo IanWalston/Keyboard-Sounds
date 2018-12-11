4ab8d8bc1c8ba3483f12c12a01d009e6deb5b928
Ian Walston <ikwalston@comcast.net>
2018-12-11 18:40:51 -0500

    made buttons dynamic

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const consonates = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
-const vowels = 'aeiouAEIOU'
-const numbers = '0123456789'
-const evens = '02468'
-const odds = '13579'
-const space = ' '
-const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"'
-const alpha = consonates + vowels
-const allCharacters = alpha + numbers + space + specialCharacters
-
+const charsets = {
+    consonates: 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ',
+    vowels: 'aeiouAEIOU',
+    numbers: '0123456789',
+    evens: '02468',
+    odds: '13579',
+    space: ' ',
+    specialCharacters: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"',
+    alpha: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    allCharacters: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"'
+}
 
 const paths = {
     kick: 'sounds/808/kicks/808-Kicks01.wav',
@@ -33,14 +34,13 @@ function ProfileElement(propertyName, charSet, soundName, soundPath) {
 
 //this constructs profile elements within the sound profile
 ///ProfileElement(propertyName, charSet, soundName, soundPath) 
-new ProfileElement("consonates", consonates, "hat", paths.hat)
-new ProfileElement("vowels", vowels, "snare", paths.snare)
-new ProfileElement("evens", evens, "maraca1", paths.maraca1)
-new ProfileElement("odds", odds, "maraca2", paths.maraca2)
-new ProfileElement("space", space, "kick", paths.kick)
-new ProfileElement("specialCharacters", specialCharacters, "Bell", paths.bell)
+new ProfileElement("consonates", charsets.consonates, "hat", paths.hat)
+new ProfileElement("vowels", charsets.vowels, "snare", paths.snare)
+new ProfileElement("evens", charsets.evens, "maraca1", paths.maraca1)
+new ProfileElement("odds", charsets.odds, "maraca2", paths.maraca2)
+new ProfileElement("space", charsets.space, "kick", paths.kick)
+new ProfileElement("specialCharacters", charsets.specialCharacters, "Bell", paths.bell)
 
-console.log(soundProfile)
 
 //////soundHash - this will be filled with a property:value pair for each keyboard character.
 //each property is a character and each value is a path to a .wav file
@@ -49,24 +49,40 @@ let soundHash = {
     other: 'sounds/808/percussion/808-Cowbell1.wav'
 }
 
-//defining functions for adding the sound profile to the sound hash
+//defining functions that will associate chars from a string with a sound path in the sound hash
 let addStringtoSoundHash = (string, path) => {
     for (let char of string) {
         soundHash[char] = path
     }
 }
+
+//defining function that will assign sounds from sound profile to sound hash
 let addSoundProfiletoSoundHash = () => {
-    //Assigning sounds from soundProfile to the soundHash
     Object.keys(soundProfile).forEach((obj) => {
         addStringtoSoundHash(soundProfile[obj].charSet, soundProfile[obj].soundPath)
     })
 }
 
+var makeSoundPathButtons = () => {
+    var div = document.querySelector("#soundPathButtonsDiv")
+    var arr = []
+    Object.keys(paths).forEach(key => (arr.push(key)))
+    arr.forEach(path=>div.innerHTML+=`<button onclick="assignUserInputtoSoundHash(paths.${path})">${path}</button>`)
+}
+var makeCharsetButtons = () => {
+    var div = document.querySelector("#charSetButtonsDiv")
+    var arr = []
+    Object.keys(charsets).forEach(key => (arr.push(key)))
+    arr.forEach(charset=>{div.innerHTML+=`<button onclick="newCharSet(charsets.${charset})">${charset}</button>`})
+    
+}
 //adding default sound profile to sound hash
 addSoundProfiletoSoundHash()
+makeCharsetButtons()
+makeSoundPathButtons()
 
 //setting form value  to userInput
-userInput = document.querySelector("#newCharSet")
+userInput = document.querySelector("#userInput")
 
 //this function is for buttons that set sounds to keyboard keys using the user inputted string
 let assignUserInputtoSoundHash = (path) => {
@@ -83,6 +99,7 @@ let newCharSet = (premadeCharSet) => {
 //KEYPRESS EVENT
 document.onkeydown = (e) => {
 
+    
     if (soundHash.hasOwnProperty(e.key)) {
         let audioElement = document.createElement("audio")
         audioElement.setAttribute("src", soundHash[e.key])
@@ -93,14 +110,6 @@ document.onkeydown = (e) => {
         audioElement.setAttribute("src", soundHash.other)
         audioElement.play()
     }
-
-    if (e.key != " ") {
-        document.querySelector("#charPress").innerHTML = e.key
-    }
-    else {
-        document.querySelector("#charPress").innerHTML = '_'
-    }
-
-    document.querySelector()
-
+    
+    document.querySelector("#charPress").innerHTML = e.key
 }//end of keypress event
